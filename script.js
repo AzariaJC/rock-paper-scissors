df16736e51a91e5725f7802ea327d959fc2644ec
AzariaJC <45110670+AzariaJC@users.noreply.github.com>
2025-06-30 15:24:05 -0400

    will give "GAME OVER", "scoreboard" and scores after 5 button pushes

--- script.js
+++ script.js
@@ -13,91 +13,118 @@ function getComputerChoice() {
   }
 }
 const result = document.querySelector("#result");
-/*function getHumanChoice() {
-   let userInput = prompt("Rock, paper, or scissors?")
-    let newUserInput = userInput.toLowerCase()
-    console.log(newUserInput);
-    return newUserInput;
-   }     THIS IS FOR THE OLD VERSION W/O A UI*/
 
 rockButton.addEventListener("click", () => {
+  clickCount++;
   humanChoice = "rock";
   computerChoice = getComputerChoice();
   playGame(humanChoice, computerChoice);
   const outcome = resultOfRound(humanChoice, computerChoice);
-  const span = document.createElement("span")
-  result.appendChild(span);
-  span.textContent = outcome;
+  const listItem = document.createElement("li");
+  result.appendChild(listItem);
+  listItem.textContent = outcome;
+  if (clickCount === 5 && computerScore > humanScore) {
+   const gameEndSection = document.querySelector("#gameEndSection");
+   const endMessage = document.createElement("p");
+   const endComputerScore = document.createElement("li")
+   const endHumanScore = document.createElement("li");
+   const scoreDisplay = document.createElement("ul");
+   const finalDeclare = document.createElement("p");
+
+   endMessage.textContent = "GAME OVER"
+   scoreDisplay.textContent = "Scoreboard"
+   endComputerScore.textContent = computerScore;
+   endHumanScore.textContent = humanScore;
+   finalDeclare.textContent = "COMPUTER WINS"
+
+   
+   gameEndSection.appendChild(endMessage);
+   gameEndSection.appendChild(scoreDisplay);
+   scoreDisplay.appendChild(endComputerScore);
+   scoreDisplay.appendChild(endHumanScore);
+  } else if (clickCount === 5 && computerScore < humanScore) {
+   const gameEndSection = document.querySelector("#gameEndSection");
+   const endMessage = document.createElement("p");
+   const endComputerScore = document.createElement("li")
+   const endHumanScore = document.createElement("li");
+   const scoreDisplay = document.createElement("ul");
+   const finalDeclare = document.createElement("p");
+
+   endMessage.textContent = "GAME OVER"
+   scoreDisplay.textContent = "Scoreboard"
+   endComputerScore.textContent = computerScore;
+   endHumanScore.textContent = humanScore;
+   finalDeclare.textContent = "YOU WIN"
+
+   
+   gameEndSection.appendChild(endMessage);
+   gameEndSection.appendChild(scoreDisplay);
+   scoreDisplay.appendChild(endComputerScore);
+   scoreDisplay.appendChild(endHumanScore);
+  }
 });
 
 paperButton.addEventListener("click", () => {
+  clickCount++;
   humanChoice = "paper";
   computerChoice = getComputerChoice();
   playGame(humanChoice, computerChoice);
+  const outcome = resultOfRound(humanChoice, computerChoice);
+  const listItem = document.createElement("li");
+  result.appendChild(listItem);
+  listItem.textContent = outcome;
 });
 
 scissorsButton.addEventListener("click", () => {
+  clickCount++;
   humanChoice = "scissors";
   computerChoice = getComputerChoice();
   playGame(humanChoice, computerChoice);
+  const outcome = resultOfRound(humanChoice, computerChoice);
+  const listItem = document.createElement("li");
+  result.appendChild(listItem);
+  listItem.textContent = outcome;
 });
-
-
-
-
 
 let humanScore = 0;
 let computerScore = 0;
+let clickCount = 0;
 
 function evalChoices(humanChoice, computerChoice) {
-  if (humanChoice == "rock" && computerChoice == "paper") {
+  if (
+    (humanChoice == "rock" && computerChoice == "paper") ||
+    (humanChoice == "paper" && computerChoice == "scissors") ||
+    (humanChoice == "scissors" && computerChoice == "rock")
+  ) {
     computerScore++;
-  } else if (humanChoice == "paper" && computerChoice == "rock") {
-    humanScore++;
-  } else if (humanChoice == "rock" && computerChoice == "scissors") {
-    humanScore++;
-  } else if (humanChoice == "scissors" && computerChoice == "rock") {
+  } else if (humanChoice == computerChoice) {
     computerScore++;
-  } else if (humanChoice == "paper" && computerChoice == "scissors") {
-    computerScore++;
-  } else if (humanChoice == "scissors" && computerChoice == "paper") {
     humanScore++;
   } else {
-    console.log("DRAW");
+    humanScore++;
   }
 }
 
 function resultOfRound(humanChoice, computerChoice) {
-   let humanWin = "You Won";
-   let computerWin = "You Lost";
-   let draw = "It's a tie"
-   if (humanChoice == "rock" && computerChoice == "paper" || 
-      humanChoice == "paper" && computerChoice == "scissors" || 
-      humanChoice == "scissors" && computerChoice == "rock") {
-         return computerWin;
-      } else if (humanChoice == computerChoice) {
-         return draw;
-      }  else {
-         return humanWin;
-      }
-};
+  let humanWin = "You Won";
+  let computerWin = "You Lost";
+  let draw = "It's a tie";
+  if (
+    (humanChoice == "rock" && computerChoice == "paper") ||
+    (humanChoice == "paper" && computerChoice == "scissors") ||
+    (humanChoice == "scissors" && computerChoice == "rock")
+  ) {
+    return computerWin;
+  } else if (humanChoice == computerChoice) {
+    return draw;
+  } else {
+    return humanWin;
+  }
+}
 
-
-
-/*function playRound() { //I don't think I need this anymore, I think evalChoices should be suffice
-  let humanSelection = getHumanChoice();
-  let computerSelection = getComputerChoice();
-  evalChoices(humanSelection, computerSelection);
-}*///need to change this function, getHumanChoice() doesn't exist anymore w/UI
-
-function playGame(humanChoice, computerChoice) { //this is almost certainly going to have to be reworked. This function needs to be triggered when you click a button.
+function playGame(humanChoice, computerChoice) {
+  //this is almost certainly going to have to be reworked. This function needs to be triggered when you click a button.
   evalChoices(humanChoice, computerChoice); //changed this to evalChoices instead of playRound
   //need to define humanChoice
   resultOfRound(humanChoice, computerChoice);
-  if (humanScore > computerScore) {
-    console.log("You won!");
-  } else {
-    console.log("You lost.");
-  }
-};
-
+}
